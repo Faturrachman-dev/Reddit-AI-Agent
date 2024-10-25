10e4efd6d36eb8d1bf9eeaafd555da88ba6a8a2a
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2024-10-25 23:15:35 +0530

    Added UI and fetch threads working!!

--- main2.py
+++
@@ -1,7 +0,0 @@
-import tweepy
-
-client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAAwWwgEAAAAAEx1MNFvrRnz07gZkUNaEQU0jyIg%3DtwiKDKTol20Fpe1D4sNdXVwtLcK87IFhlIqHvKsT5rmPvcCP26")
-
-tweets = client.search_recent_tweets(query="generative ai", max_results=3)
-
-print(tweets)
